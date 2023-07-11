21c40790b665e7314eba8ebdea613def524c3c39
mimi5963 <mimi5963@naver.com>
2023-07-11 18:23:19 +0900

    logic80%

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,12 +1,13 @@
 public class BaseballGame {
     private final String RANDOM_NUMBER;
-
+    private int[] counter;
       BaseballGame(){
         String random="";
         for(int i=0; i<3;i++) {
            random += ((int) (Math.random() * 8) + 1)+"";
         }
         RANDOM_NUMBER = random;
+        counter = new int[2];
     }
 
     public String getRandomNumber() {
@@ -15,6 +16,28 @@ public class BaseballGame {
 
 
     public boolean checkUserInput(String userInput) {
-          
+          //숫자만 같으면 볼
+          //숫자와 위치가 같으면 스트라이크
+
+      //  for
+
+        return false;
     }
+
+    private int findNumber(char userInputChar){
+          return RANDOM_NUMBER.indexOf(userInputChar);
+    }
+    private boolean isStrike(int findNumberidx, int i){
+          if(findNumberidx == i)
+              return true;
+
+          return false;
+    }
+    private void setBallAndStrike(int findNumberidx,boolean isStrike){
+          if(isStrike)
+              counter[0]++;
+          else if(findNumberidx != -1)
+              counter[1]++;
+    }
+
 }
--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.testng.Assert;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,10 +27,78 @@ class BaseballGameTest {
         String num = newgame.getRandomNumber();
         Assert.assertEquals(num.length(),3);
     }
-
     @AfterEach
     void tearDown(){
         newgame =null;
     }
+
+
+  @Test
+  void resultBallAndStrike(){
+       int[] test = ballAndStrikeTest("213","213");
+       assertArrayEquals(new int[]{3,0},test);
+  }
+    @Test
+    void isStrikeTest(){
+        assertFalse(isStrike(-1,1));
+        assertFalse(isStrike(1,2));
+        assertTrue(isStrike(1,1));
+        assertTrue(isStrike(2,2));
+    }
+    @Test
+    void  intArrayPulsTest(){
+        int[] one = new int[] {0,0};
+        intArrPuls(one,(int[] i)->{i[0]++; i[1]++;});
+        assertArrayEquals(new int[]{1,1}, one);
+    }
+    @Test
+    void SetResultTest(){
+        int[] t = new int[]{0,0};
+        setResult(0,true,t);
+
+    }
+    
+  private int[] ballAndStrikeTest(String computer, String user){
+        int idx;
+        boolean isStrike=false;
+        int[] result = new int[]{0,0};
+        for(int i=0;i<computer.length();i++){
+             idx = findnumber(computer,user.charAt(i));
+            isStrike= isStrike(idx,i);
+             setResult(idx,isStrike,result);
+        }
+
+        return result;
+    }
+
+    private void setResult(int idx, boolean isStrike, int[] result) {
+        if(isStrike && idx != -1)
+            result[0]++;
+        else if(idx != -1)
+            result[1]++;
+
+
+    }
+
+    private boolean isStrike(int ballidx, int i) {
+      if(ballidx == i)
+          return true;
+
+      return false;
+    }
+
+
+    private int findnumber(String conputerInput, char Userinput){
+            return conputerInput.indexOf(Userinput);
+    }
+
+
+
+    private void intArrPuls(int[] one,Consumer<int[]> o) {
+        o.accept(one);
+    }
+
+
+
 
 }
